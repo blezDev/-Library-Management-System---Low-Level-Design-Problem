1c1232b6a2282dcdb7eaf83a033f7e48ec437d31
blezDev <msaikrishnapatnaik@gmail.com>
2024-10-14 21:57:44 +0530

    Refining

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Librarian librarian = new Librarian();
+        librarian.addBook(new BookItem(new Date(),"123213421kvbkhv", "The Catcher in the Rye", "J.D. Salinger", "1951"));
+        librarian.addPatron(new Patron("123", "John Doe", new Date()));
+        librarian.lendBook("123213421kvbkhv", "123");
+        System.out.println(librarian.getPatronHistory("123"));
+
     }
 }
